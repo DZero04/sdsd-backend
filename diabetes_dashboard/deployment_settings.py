64f66310afa1fadde0201aaea33912aaf0cfcf5f
DZero04 <danmarlouarevalo@gmail.com>
2025-01-06 00:59:10 +0800

    cors headers

--- diabetes_dashboard/deployment_settings.py
+++ diabetes_dashboard/deployment_settings.py
@@ -21,9 +21,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://sdsd-frontend.onrender.com'
+]
 
 STORAGES = {
     "default": {
