33d3e556f79ea320657e9e9505a6716137848ac8
DZero04 <danmarlouarevalo@gmail.com>
2025-01-06 01:52:50 +0800

    last na pleaaaase

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ python manage.py migrate
 # Transfer old data from SQLite to PostgreSQL
 # Check if the data.json file exists
 if [ -f "regions.json" ]; then
-    echo "Loading data from data.json into the database..."
-    python manage.py loaddata data.json
+    echo "Loading data from regions.json into the database..."
+    python manage.py loaddata regions.json
 else
-    echo "No data.json file found. Skipping data import."
+    echo "No regions.json file found. Skipping data import."
 fi
