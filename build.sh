330cafb5df0dc189ee99a793150a88287b890096
DZero04 <danmarlouarevalo@gmail.com>
2025-01-06 02:01:12 +0800

    fix files

--- build.sh
+++ build.sh
@@ -5,12 +5,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-# Transfer old data from SQLite to PostgreSQL
-# Check if the data.json file exists
-if [ -f "regions.json" ]; then
-    echo "Loading data from regions.json into the database..."
-    python manage.py loaddata regions.json
-else
-    echo "No regions.json file found. Skipping data import."
-fi
