8ffc664a147800aa231833ebc790bb125d3a68a2
DZero04 <danmarlouarevalo@gmail.com>
2025-01-06 01:33:51 +0800

    changes in database

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+# Transfer old data from SQLite to PostgreSQL
+# Check if the data.json file exists
+if [ -f "data.json" ]; then
+    echo "Loading data from data.json into the database..."
+    python manage.py loaddata data.json
+else
+    echo "No data.json file found. Skipping data import."
+fi
