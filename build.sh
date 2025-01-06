6c8b5a94c0ad387f7ab75622e66c32c6a22be49f
DZero04 <danmarlouarevalo@gmail.com>
2025-01-06 17:25:29 +0800

    try loading tables

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py loaddata data_diabetesdata.json
+python manage.py loaddata data_gender.json
+python manage.py loaddata data_region.json
+python manage.py loaddata data_results.json
